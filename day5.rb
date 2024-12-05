7ff0a7489752c3de61987a65e00a020893556866
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-05 13:36:00 -0500

    Add basic input parsing and testing

--- day5.rb
+++ day5.rb
@@ -1,91 +1,91 @@
 #!/usr/bin/env ruby
 
-FILENAME = "./day4_input.txt"
+require "minitest/autorun"
+FILENAME = "./day5_input.txt"
 
 real_input = File.read(FILENAME)
 
-test = <<-TEST
-MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX
+TEST= <<-TEST
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
 TEST
 
-def convert_to_grid(text)
-  text.each_line.map do |line|
-    line.chomp.each_char.to_a
-  end
-end
-
-def xmas_or_samx?(arr)
-  [%w(X M A S), %w(S A M X)].include?(arr)
-end
-
-def count_xmas(grid)
-  horizontal = grid.sum do |line|
-    line.each_cons(4).count(&method(:xmas_or_samx?))
-  end
-
-  vertical = grid.transpose.sum do |col|
-    col.each_cons(4).count(&method(:xmas_or_samx?))
-  end
-
-  diag_dr_ul = grid.each_cons(4).sum do |row1,row2,row3,row4|
-    grid.first.each_index.each_cons(4).count do |j1,j2,j3,j4|
-      xmas_or_samx?([row1[j1],row2[j2],row3[j3],row4[j4]])
-    end
-  end
-
-  diag_ur_dl = grid.each_cons(4).sum do |row1,row2,row3,row4|
-    grid.first.each_index.each_cons(4).count do |j1,j2,j3,j4|
-      xmas_or_samx?([row1[j4],row2[j3],row3[j2],row4[j1]])
-    end
-  end
-
+def input_to_deps_and_updates(text)
+  deps_text, updates_text = text.split("\n\n")
   [
-    horizontal,
-    vertical,
-    diag_dr_ul,
-    diag_ur_dl,
-  ].sum
-end
-
-#puts "TEST RESULT: #{count_xmas(convert_to_grid(test))}"
-puts "RESULT part 1: #{count_xmas(convert_to_grid(real_input))}"
-
-
-def extract_cross(grid, i_start, j_start)
-  [
-    grid[i_start][j_start],
-    grid[i_start][j_start+2],
-    grid[i_start+1][j_start+1],
-    grid[i_start+2][j_start],
-    grid[i_start+2][j_start+2],
+    deps_text.each_line.map { |l| l.scan(/\d+/) },
+    updates_text.each_line.map { |l| l.scan(/\d+/) },
   ]
 end
 
-def check_x_mas(grid, i_start, j_start)
-  [
-    %w(M M A S S),
-    %w(S S A M M),
-    %w(M S A M S),
-    %w(S M A S M),
-  ].include?(extract_cross(grid, i_start, j_start))
-end
+class TestInputParse < Minitest::Test
+  def test_input_parse_deps
+    assert_equal(
+      [
+        %w(47 53),
+        %w(97 13),
+        %w(97 61),
+        %w(97 47),
+        %w(75 29),
+        %w(61 13),
+        %w(75 53),
+        %w(29 13),
+        %w(97 29),
+        %w(53 29),
+        %w(61 53),
+        %w(97 53),
+        %w(61 29),
+        %w(47 13),
+        %w(75 47),
+        %w(97 75),
+        %w(47 61),
+        %w(75 61),
+        %w(47 29),
+        %w(75 13),
+        %w(53 13),
+      ],
+      input_to_deps_and_updates(TEST)[0]
+    )
+  end
 
-def count_x_mas(grid)
-  grid.each_index.each_cons(3).sum do |i,_,_|
-    grid.first.each_index.each_cons(3).count do |j,_,_|
-      check_x_mas(grid, i, j)
-    end
+  def test_input_parse_updates
+    assert_equal(
+      [
+        %w(75 47 61 53 29),
+        %w(97 61 53 29 13),
+        %w(75 29 13),
+        %w(75 97 47 61 53),
+        %w(61 13 29),
+        %w(97 13 75 29 47),
+      ],
+      input_to_deps_and_updates(TEST)[1]
+    )
   end
 end
 
-#puts "TEST X-MAS: #{count_x_mas(convert_to_grid(test))}"
-puts "RESULT part 2: #{count_x_mas(convert_to_grid(real_input))}"
