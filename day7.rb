adfe5199549fe37afd64353cbfcd0bd36f1d9b44
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-07 17:31:49 -0500

    day7

--- day7.rb
+++ day7.rb
@@ -1,91 +1,102 @@
 #!/usr/bin/env ruby
 
-FILENAME = "./day4_input.txt"
+require "minitest/autorun"
+
+FILENAME = "./day7_input.txt"
 
 real_input = File.read(FILENAME)
 
-test = <<-TEST
-MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX
+test_data = <<-TEST
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
 TEST
 
-def convert_to_grid(text)
+def parse_to_result_and_operands(text)
   text.each_line.map do |line|
-    line.chomp.each_char.to_a
+    ary = line.scan(/\d+/).map(&:to_i)
+    [ary.first, ary[1..]]
   end
 end
 
-def xmas_or_samx?(arr)
-  [%w(X M A S), %w(S A M X)].include?(arr)
+def is_possible_p1(result, operands)
+  if operands.length == 1
+    return result == operands.first
+  end
+  first, second = operands[0], operands[1]
+  is_possible_p1(result, [first + second] + operands[2..]) ||
+    is_possible_p1(result, [first * second] + operands[2..])
 end
 
-def count_xmas(grid)
-  horizontal = grid.sum do |line|
-    line.each_cons(4).count(&method(:xmas_or_samx?))
+def is_possible_p2(result, operands)
+  if operands.length == 1
+    return result == operands.first
   end
-
-  vertical = grid.transpose.sum do |col|
-    col.each_cons(4).count(&method(:xmas_or_samx?))
-  end
-
-  diag_dr_ul = grid.each_cons(4).sum do |row1,row2,row3,row4|
-    grid.first.each_index.each_cons(4).count do |j1,j2,j3,j4|
-      xmas_or_samx?([row1[j1],row2[j2],row3[j3],row4[j4]])
-    end
-  end
-
-  diag_ur_dl = grid.each_cons(4).sum do |row1,row2,row3,row4|
-    grid.first.each_index.each_cons(4).count do |j1,j2,j3,j4|
-      xmas_or_samx?([row1[j4],row2[j3],row3[j2],row4[j1]])
-    end
-  end
-
-  [
-    horizontal,
-    vertical,
-    diag_dr_ul,
-    diag_ur_dl,
-  ].sum
+  first, second = operands[0], operands[1]
+  is_possible_p2(result, [first + second] + operands[2..]) ||
+    is_possible_p2(result, [first * second] + operands[2..]) ||
+    is_possible_p2(result, [(first.to_s + second.to_s).to_i] + operands[2..])
 end
 
-#puts "TEST RESULT: #{count_xmas(convert_to_grid(test))}"
-puts "RESULT part 1: #{count_xmas(convert_to_grid(real_input))}"
-
-
-def extract_cross(grid, i_start, j_start)
-  [
-    grid[i_start][j_start],
-    grid[i_start][j_start+2],
-    grid[i_start+1][j_start+1],
-    grid[i_start+2][j_start],
-    grid[i_start+2][j_start+2],
-  ]
+def compute_result_p1(results_and_operands)
+  results_and_operands.map do |result, operands|
+    next(result) if is_possible_p1(result, operands)
+    0
+  end.sum
 end
 
-def check_x_mas(grid, i_start, j_start)
-  [
-    %w(M M A S S),
-    %w(S S A M M),
-    %w(M S A M S),
-    %w(S M A S M),
-  ].include?(extract_cross(grid, i_start, j_start))
+def compute_result_p2(results_and_operands)
+  results_and_operands.map do |result, operands|
+    next(result) if is_possible_p2(result, operands)
+    0
+  end.sum
 end
 
-def count_x_mas(grid)
-  grid.each_index.each_cons(3).sum do |i,_,_|
-    grid.first.each_index.each_cons(3).count do |j,_,_|
-      check_x_mas(grid, i, j)
-    end
+Class.new(Minitest::Test) do
+  define_method :test_parse_to_result_and_operands do
+    assert_equal(
+      [
+        [190, [10,19]],
+        [3267, [81, 40, 27]],
+        [83, [17, 5]],
+        [156, [15, 6]],
+        [7290, [6, 8, 6, 15]],
+        [161011, [16, 10, 13]],
+        [192, [17, 8, 14]],
+        [21037, [9, 7, 18, 13]],
+        [292, [11, 6, 16, 20]],
+      ],
+      parse_to_result_and_operands(test_data)
+    )
+  end
+
+  define_method :test_compute_result_p1 do
+    assert_equal(
+      3749,
+      compute_result_p1(parse_to_result_and_operands(test_data)),
+    )
+
+    assert_equal(
+      6392012777720,
+      compute_result_p1(parse_to_result_and_operands(real_input)),
+    )
+  end
+
+  define_method :test_compute_result_p2 do
+    assert_equal(
+      11387,
+      compute_result_p2(parse_to_result_and_operands(test_data)),
+    )
+
+    assert_equal(
+      61561126043536,
+      compute_result_p2(parse_to_result_and_operands(real_input)),
+    )
   end
 end
-
-#puts "TEST X-MAS: #{count_x_mas(convert_to_grid(test))}"
-puts "RESULT part 2: #{count_x_mas(convert_to_grid(real_input))}"
