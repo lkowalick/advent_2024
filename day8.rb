e5ca06ddcf954704c455f62c0ee3545e943bcb3b
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-08 12:49:29 -0500

    Finish input parsing

--- day8.rb
+++ day8.rb
@@ -25,13 +25,18 @@ Result = Struct.new(:height, :width, :stations)
 def parse_input(text)
   height = text.each_line.count
   width = 0
-  text.each_line do |line|
-    width = line.chomp.length
-    break
+  stations = Hash.new { |h, k| h[k] = [] }
+  text.each_line.each_with_index do |line, i|
+    line.chomp!
+    width = line.length
+    line.each_char.each_with_index do |char, j|
+      next if char == "."
+      stations[char] << [i,j]
+    end
   end
 
 
-  Result.new(height, width, [])
+  Result.new(height, width, stations)
 end
 
 
@@ -45,6 +50,15 @@ Class.new(Minitest::Test) do
     assert_equal(
       12,
       parse_input(test_data).width,
+    )
+
+    assert_equal(
+      [[1,8],[2,5],[3,7],[4,4]],
+      parse_input(test_data).stations["0"]
+    )
+    assert_equal(
+      [[5,6],[8,8],[9,9]],
+      parse_input(test_data).stations["A"]
     )
   end
 end
