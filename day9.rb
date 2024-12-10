505b15f7756336f96e119e3337befa4729cfafdd
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-09 19:42:35 -0500

    Super basic input parsing

---
+++ day9.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+require "minitest/autorun"
+
+FILENAME = "./day9_input.txt"
+
+real_input = File.read(FILENAME)
+
+test_data = <<-TEST
+2333133121414131402
+TEST
+
+ParsedInput = Struct.new(:disk, :free_space, :ids)
+def parse_input(text)
+  disk = []
+  free_space = 0
+  ids = Set.new()
+  text.chomp.each_char.each_slice(2).with_index do |slice, id|
+    ids << id.to_s
+    block_size = slice[0].to_i
+    block_size.times { disk << id }
+    next unless slice[1]
+    free_size = slice[1].to_i
+    free_size.times { disk << -1 }
+    free_space += free_size
+  end
+
+  ParsedInput.new(disk, free_space, ids.to_a)
+end
+
+
+
+
+Class.new(Minitest::Test) do
+  define_method :test_parse_input do
+    assert_equal(
+      %w(0 1 2 3 4 5 6 7 8 9),
+      parse_input(test_data).ids
+    )
+    assert_equal(
+      14,
+      parse_input(test_data).free_space,
+    )
+  end
+end
