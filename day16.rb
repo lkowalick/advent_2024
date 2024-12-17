bdfcafca5aebaba7a8b29074c5fd11bfe19ca212
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-17 02:28:50 -0500

    stuck on part 2

--- day16.rb
+++ day16.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require "minitest/autorun"
+require 'algorithms'
 
 FILENAME = "./day16_input.txt"
 
@@ -46,26 +47,41 @@ TEST
 
 
 def parse_input(text)
-  pos = nil
+  pos, finish = nil, nil
   maze = text.each_line.with_index.map do |line, i|
     line.chomp.each_char.with_index.map do |char, j|
       case char
       when "S"
         pos = [i,j]
         "."
+      when "E"
+        finish = [i,j]
+        char
       else
         char
       end
     end
   end
   facing = 0
-  { maze:, pos:, facing: }
+  { maze:, pos:, facing:, finish: }
 end
 
-def solve(maze)
-  maze => { maze:, pos: ,facing: }
+def solve(input)
+  input => { maze:, pos: ,facing: }
   solve_helper(maze, [pos], facing)
 end
+
+def solve_with_limit(input, limit)
+  input => { maze:, pos: ,facing: }
+  solve_helper(maze, [pos], facing, limit)
+end
+
+#def get_seats(input)
+#  limit = dijkstra(input)
+#  solve_with_limit(input, limit).flatten.each_with_object(Set.new) do |pos, result|
+#    result << pos
+#  end.length
+#end
 
 
 def get_cost(path)
@@ -98,24 +114,16 @@ def travel(pos, dir)
   [x+dx, y+dy]
 end
 
-def solve_helper(maze, path, dir)
-  neighbors(maze,path,dir).map do |i,j,dir|
-    local_path = path.dup
-    local_path << [i,j]
-    return local_path if maze[i][j] == "E"
-    recursive_sol = solve_helper(maze, local_path, dir)
-    recursive_sol if recursive_sol
-  end.compact.min_by { |path| get_cost(path) }
-end
 
-def maze_to_string(maze)
-  maze => { pos: , maze:, facing: }
+
+def maze_to_string(input)
+  input => { pos: , maze:, facing: }
   "position: #{pos.inspect} | facing: #{facing}\n" + maze.map { |row| row.join("") }.join("\n")
 end
 
-def solution_to_string(maze)
-  solution = solve(maze)
-  maze => { pos: , maze:, facing: }
+def solution_to_string(input)
+  solution = solve(input)
+  input => { pos: , maze:, facing: }
   maze.each_with_index.map do |row, i|
     row.each_with_index.map do |char, j|
       next("@") if solution.include?([i,j])
@@ -132,6 +140,129 @@ def neighbors(maze, path, dir)
     [i+dx, j+dy, current_dir]
   end.filter do |x,y,_|
     0 <= x && x < maze.length && 0 <= y && y < maze.first.length && maze[x][y] != "#" && !path.include?([x,y])
+  end
+end
+
+def get_reachable_vertices(input)
+  input => { maze: , pos:, facing: }
+  seen = Set.new()
+  queue = [[pos, facing]]
+  until queue.empty? do
+    location, dir = queue.pop
+    next if seen.include?([location,dir])
+    seen << [location,dir]
+    dij_neighbors(maze, location,dir).each do |neighbor|
+      queue << neighbor
+    end
+  end
+  seen
+end
+
+def dij_neighbors(maze, pos, dir)
+  neighbors = [[pos,(dir+1)%4],[pos,(dir+3)%4]]
+  x, y = pos
+  dx, dy = DIR_OFFSETS.fetch(dir)
+  adj_neighbors = [
+    [[x+dx, y+dy], dir],
+    [[x-dx, y-dy], dir],
+  ].filter do |(x,y),_|
+    maze[x][y] != "#"
+  end
+  neighbors + adj_neighbors
+end
+
+def dijkstra(input)
+  input => { pos:, maze: , facing:, finish:}
+  dist = {}
+  queue = Containers::MinHeap.new
+  seen = Set.new
+  get_reachable_vertices(input).each do |vertex|
+    dist[vertex] = Float::INFINITY
+    queue.push(dist[vertex], vertex)
+  end
+
+  dist[[pos,facing]] = 0
+  dist[[pos,(facing+1)%4]] = Float::INFINITY
+  dist[[pos,(facing+2)%4]] = Float::INFINITY
+  dist[[pos,(facing+3)%4]] = Float::INFINITY
+
+  until queue.empty?
+    u = queue.pop
+    pos, dir = u
+    next if seen.include?(u)
+    seen << u
+
+    dij_neighbors(maze, *u).each do |neighbor|
+      neighbor_pos, neighbor_dir = neighbor
+      next if seen.include?(neighbor)
+      cost = if neighbor_dir == dir
+               1
+             else
+               1000
+             end
+      new_dist = dist.fetch(u) + cost
+       if new_dist < dist.fetch(neighbor)
+         dist[neighbor] = new_dist
+         queue.push(new_dist, neighbor)
+       end
+    end
+  end
+
+
+  [
+    0.upto(3).map do |dir|
+      dist[[finish, dir]]
+    end.min,
+    dist,
+  ]
+end
+
+def get_seats(input)
+  input => { maze:, facing: , pos:}
+  min, dist = dijkstra(input)
+
+
+  set = Set.new
+  solve_helper_good(maze, Set.new,[pos,facing] , dist, min).each_slice(3) do |i,j,k|
+    raise if i.nil? || j.nil? || k.nil?
+    set << [i,j]
+  end
+  puts ""
+  puts(maze.each_with_index.map do |row, i|
+    row.each_with_index.map do |char, j|
+      next("O") if set.include?([i,j])
+      char
+    end.join("")
+  end.join("\n"))
+  set.length
+end
+
+
+def solve_helper_good(maze, path, vertex, dist, min)
+  pos, dir = vertex
+  path << vertex
+  result = dij_neighbors(maze,*vertex).map do |neighbor|
+    next(nil) if path.include?(neighbor)
+    neighbor_pos, neighbor_dir = neighbor
+    cost = if neighbor_dir == dir
+             1
+           else
+             1000
+           end
+    next(nil) unless dist[vertex] + cost == dist[neighbor] &&  dist[neighbor] <= min
+
+
+    if maze.dig(*neighbor_pos) == "E"
+      path.dup << neighbor
+    else
+      solve_helper_good(maze, path, neighbor, dist, min)
+    end
+  end.compact
+  path.delete(vertex)
+  if result.empty?
+    nil
+  else
+    result.flatten
   end
 end
 
@@ -179,9 +310,18 @@ Class.new(Minitest::Test) do
       EXPECTED
   end
 
-  define_method :test_solution_cost do
-    assert_equal(7036, get_cost(solve(parse_input(test_data_1))))
-    assert_equal(11048, get_cost(solve(parse_input(test_data_2))))
+
+  define_method :test_dijkstra do
+    assert_equal(7036, dijkstra(parse_input(test_data_1)).first)
+    assert_equal(11048, dijkstra(parse_input(test_data_2)).first)
+    #assert_equal(73404, dijkstra(parse_input(real_data)).first)
+  end
+
+  define_method :test_get_seats do
+    #assert_equal(45, get_seats(parse_input(test_data_1)))
+    assert_equal(64, get_seats(parse_input(test_data_2)))
   end
 end
+
+puts "part 2: #{get_seats(parse_input(real_data))}"
 
