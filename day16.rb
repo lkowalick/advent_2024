62fb6f9cd0ab09e10df2f830bc13d657d86abacb
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-16 21:25:19 -0500

    basic solving

--- day16.rb
+++ day16.rb
@@ -47,15 +47,12 @@ TEST
 DIRECTIONS = %i(e s w n)
 
 def parse_input(text)
-  pos, finish = nil, nil
+  pos = nil
   maze = text.each_line.with_index.map do |line, i|
     line.chomp.each_char.with_index.map do |char, j|
       case char
       when "S"
         pos = [i,j]
-        "."
-      when "E"
-        finish = [i,j]
         "."
       else
         char
@@ -63,25 +60,36 @@ def parse_input(text)
     end
   end
   facing = :e
-  visited = Set.new([pos])
-  { maze:, pos:, finish:, facing:, visited: }
+  { maze:, pos:, facing: }
 end
 
 def solve(maze)
+  maze => { maze:, pos: }
+  solve_helper(maze, [pos])
+end
 
+def solve_helper(maze, path)
+  neighbors(maze,path).each do |i,j|
+    local_path = path.dup
+    local_path << [i,j]
+    return local_path if maze[i][j] == "E"
+    recursive_sol = solve_helper(maze, local_path)
+    return recursive_sol if recursive_sol
+  end
+  nil
 end
 
 def to_string(maze)
-  maze => { pos: , maze:, finish:, facing: }
-  "position: #{pos.inspect} | finish: #{finish.inspect} | facing: #{facing}\n" + maze.map { |row| row.join("") }.join("\n")
+  maze => { pos: , maze:, facing: }
+  "position: #{pos.inspect} | facing: #{facing}\n" + maze.map { |row| row.join("") }.join("\n")
 end
 
-def neighbors(maze)
-  maze => { pos: [i,j], maze:, visited: }
+def neighbors(maze, path)
+  i,j = path[-1]
   [
     [i+1,j],[i-1,j],[i,j+1],[i,j-1],
   ].filter do |x,y|
-    0 <= x && x < maze.length && 0 <= y && y < maze.first.length && maze[x][y] != "#" && !visited.include?([x,y])
+    0 <= x && x < maze.length && 0 <= y && y < maze.first.length && maze[x][y] != "#" && !path.include?([x,y])
   end
 end
 
@@ -89,9 +97,9 @@ end
 Class.new(Minitest::Test) do
   define_method :test_parse_input do
     assert_equal(<<~EXPECTED.chomp , to_string(parse_input(test_data_1)))
-      position: [13, 1] | finish: [1, 13] | facing: e
+      position: [13, 1] | facing: e
       ###############
-      #.......#.....#
+      #.......#....E#
       #.#.###.#.###.#
       #.....#.#...#.#
       #.###.#####.#.#
@@ -108,9 +116,9 @@ Class.new(Minitest::Test) do
       EXPECTED
 
     assert_equal(<<~EXPECTED.chomp , to_string(parse_input(test_data_2)))
-      position: [15, 1] | finish: [1, 15] | facing: e
+      position: [15, 1] | facing: e
       #################
-      #...#...#...#...#
+      #...#...#...#..E#
       #.#.#.#.#.#.#.#.#
       #.#.#.#...#...#.#
       #.#.#.#.###.#.#.#
@@ -130,3 +138,4 @@ Class.new(Minitest::Test) do
   end
 end
 
+puts "solution to test_data_1: #{solve((parse_input(test_data_2)))}"
