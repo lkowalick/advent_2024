94a83c8e057c929041484ad2b8c545198282aa2a
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-19 13:28:10 -0500

    Test for part 2

--- day19.rb
+++ day19.rb
@@ -22,9 +22,9 @@ TEST
 def parse_input(input)
   inventory_text, patterns_text = input.split("\n\n")
 
-  inventory = Set.new(inventory_text.scan(/\w+/))
+  inventory = inventory_text.scan(/\w+/).sort_by(&:length)
 
-  patterns = patterns_text.each_line.map { |l| l.chomp }
+  patterns = patterns_text.each_line.map { |l| l.chomp }.sort_by(&:length)
 
   { inventory:, patterns: }
 end
@@ -57,20 +57,38 @@ end
 
 def sum_ways(parsed_input)
   parsed_input => { inventory:, patterns: }
-  cache = { "" => true }
-  inventory.each { |inv| cache[inv] = true }
+  cache = Hash.new { |h,k| h[k] = 0 }
+  cache[""] = 1
+  inventory.each do |inv|
+    cache[inv] += 1
+    1.upto(inv.length - 1).each do |i|
+      cache[inv] += cache[inv[0...i]] * cache[inv[i..]]
+    end
+  end
+  inventory.each do |inv|
+    patterns.each do |pattern|
+      if pattern.end_with?(inv)
+        cache[pattern] += cache[pattern[-(inv.length)..]] * cache[inv]
+      end
+    end
+  end
 
-  patterns.sum { |p| num_ways(p, cache) }
+  patterns.sum { |p| cache[p] }
 end
 
 def num_ways(pattern, cache)
-  0
+  unless cache.key?(pattern)
+    cache[pattern] = 1.upto(pattern.length - 1).sum do |i|
+      num_ways(pattern[0...i], cache) *  num_ways(pattern[i..], cache)
+    end
+  end
+  cache.fetch(pattern).tap { |num| puts "returning #{num} for #{pattern}" }
 end
 
 Class.new(Minitest::Test) do
   define_method :test_parse_input do
-    assert_equal(%w(r wr b g bwu rb gb br), parse_input(test_data)[:inventory].to_a)
-    assert_equal(%w(brwrr bggr gbbr rrbgbr ubwu bwurrg brgr bbrgwb), parse_input(test_data)[:patterns])
+    assert_equal(%w(r wr b g bwu rb gb br).sort_by(&:length), parse_input(test_data)[:inventory].to_a)
+    assert_equal(%w(brwrr bggr gbbr rrbgbr ubwu bwurrg brgr bbrgwb).sort_by(&:length), parse_input(test_data)[:patterns])
   end
 
   define_method :test_num_possible_patterns do
