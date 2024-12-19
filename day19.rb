4cd4b250d2b46069e6ba2715f736b07b6da7e165
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-19 11:05:23 -0500

    test for part

--- day19.rb
+++ day19.rb
@@ -46,6 +46,27 @@ def pattern_possible?(pattern, cache)
   cache.fetch(pattern)
 end
 
+def pattern_possible?(pattern, cache)
+  unless cache.key?(pattern)
+    cache[pattern] = 1.upto(pattern.length - 1).any? do |i|
+      pattern_possible?(pattern[0...i], cache) && pattern_possible?(pattern[i..], cache)
+    end
+  end
+  cache.fetch(pattern)
+end
+
+def sum_ways(parsed_input)
+  parsed_input => { inventory:, patterns: }
+  cache = { "" => true }
+  inventory.each { |inv| cache[inv] = true }
+
+  patterns.sum { |p| num_ways(p, cache) }
+end
+
+def num_ways(pattern, cache)
+  0
+end
+
 Class.new(Minitest::Test) do
   define_method :test_parse_input do
     assert_equal(%w(r wr b g bwu rb gb br), parse_input(test_data)[:inventory].to_a)
@@ -55,6 +76,10 @@ Class.new(Minitest::Test) do
   define_method :test_num_possible_patterns do
     assert_equal(6, num_possible_patterns(parse_input(test_data)))
     assert_equal(242, num_possible_patterns(parse_input(real_data)))
+  end
+
+  define_method :test_sum_ways do
+    assert_equal(16, sum_ways(parse_input(test_data)))
   end
 end
 
