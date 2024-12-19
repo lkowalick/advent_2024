351911c84273267327d7592114d758804cd5df33
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-19 10:56:24 -0500

    beginnings of part 1

--- day19.rb
+++ day19.rb
@@ -21,18 +21,29 @@ TEST
 
 def parse_input(input)
   inventory_text, patterns_text = input.split("\n\n")
-  inventory = inventory_text.scan(/\w+/)
+
+  inventory = Set.new(inventory_text.scan(/\w+/))
+
   patterns = patterns_text.each_line.map { |l| l.chomp }
+
   { inventory:, patterns: }
 end
 
 def num_possible_patterns(parsed_input)
-  0
+  parsed_input => { inventory:, patterns: }
+  cache = { "" => true }
+  inventory.each { |inv| cache[inv] = true }
+
+  patterns.count { |p| pattern_possible?(p, cache) }
+end
+
+def pattern_possible?(pattern, cache)
+  false
 end
 
 Class.new(Minitest::Test) do
   define_method :test_parse_input do
-    assert_equal(%w(r wr b g bwu rb gb br), parse_input(test_data)[:inventory])
+    assert_equal(%w(r wr b g bwu rb gb br), parse_input(test_data)[:inventory].to_a)
     assert_equal(%w(brwrr bggr gbbr rrbgbr ubwu bwurrg brgr bbrgwb), parse_input(test_data)[:patterns])
   end
 
