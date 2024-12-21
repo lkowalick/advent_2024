615374c0e2c801fc6907a63a9cdcf69ce76884fb
Lindsay Kowalick <lpkowalick@gmail.com>
2024-12-21 14:55:02 -0500

    superclass

--- day21.rb
+++ day21.rb
@@ -7,38 +7,16 @@ REAL_DATA = %w(319A 985A 340A 489A 964A)
 
 TEST_DATA = %w(029A 980A 179A 456A 379A)
 
-class NumPad
-  #   7   8   9
-  #   4   5   6
-  #   1   2   3
-  #       0   A
-  #
-  #  Go up before going left
-  #  decrement i before decrementing j
-  #  Go right before going down
-  #  increment j before incrementing i
-  ENTRIES = {
-    "7" => [0,0],
-    "8" => [0,1],
-    "9" => [0,2],
-    "4" => [1,0],
-    "5" => [1,1],
-    "6" => [1,2],
-    "1" => [2,0],
-    "2" => [2,1],
-    "3" => [2,2],
-    "0" => [3,1],
-    "A" => [3,2],
-  }
 
+class EntryPad
   attr_accessor :position, :moves
   def initialize
-    @position = [3,2]
+    @position = self.class.start_position
     @moves = []
   end
 
   def enter(entry)
-    new_position = ENTRIES.fetch(entry)
+    new_position = self.class.entries.fetch(entry)
     di = new_position.first - position.first
     dj = new_position.last - position.last
 
@@ -50,6 +28,65 @@ class NumPad
 
     self.position = new_position
     moves
+  end
+end
+
+class NumPad < EntryPad
+  #   7   8   9
+  #   4   5   6
+  #   1   2   3
+  #       0   A
+  #
+  #  Go up before going left
+  #  decrement i before decrementing j
+  #  Go right before going down
+  #  increment j before incrementing i
+  class << self
+    def entries
+      {
+        "7" => [0,0],
+        "8" => [0,1],
+        "9" => [0,2],
+        "4" => [1,0],
+        "5" => [1,1],
+        "6" => [1,2],
+        "1" => [2,0],
+        "2" => [2,1],
+        "3" => [2,2],
+        "0" => [3,1],
+        "A" => [3,2],
+      }
+    end
+
+    def start_position
+      [3,2]
+    end
+  end
+end
+
+class ArrowKeys < EntryPad
+  #       ^   A
+  #   <   v   >
+  #
+  #  Go right before going up
+  #  increment j before decrementing i
+  #  Go down before going left
+  #  increment i before decrementing j
+
+  class << self
+    def entries
+      {
+        "^" => [0,1],
+        "A" => [0,2],
+        "<" => [1,0],
+        "v" => [1,1],
+        ">" => [1,2],
+      }
+    end
+
+    def start_position
+      [0,2]
+    end
   end
 end
 
